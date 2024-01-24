4e5f14beb502d7204f1837c4b14319a9e71d4e01
manthysbr <121950847+manthysbr@users.noreply.github.com>
2024-01-24 17:58:41 -0300

    agora vai

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var rootCmd = &cobra.Command{
 
 func main() {
 	// Adiciona o comando 'check' como um subcomando de 'rootCmd'
-	rootCmd.AddCommand(check.CheckCmd)
+	rootCmd.AddCommand(validate.CheckCmd)
 
 	// Execute o comando raiz e todos os subcomandos associados
 	if err := rootCmd.Execute(); err != nil {
