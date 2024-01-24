dea71ff8d931eddff2a00dc9cd1d65902df16d28
manthysbr <121950847+manthysbr@users.noreply.github.com>
2024-01-24 13:44:54 -0300

    TEST

--- cmd/help/ask_for_help.go
+++ cmd/help/ask_for_help.go
@@ -7,11 +7,10 @@ func PrintHelp() {
 
 The commands are:
 
-    check   Check if the software is already installed. 
-            Example: manthys check <software_name>
+    check   Run a check to see if the software is already installed and save the result in the software.json file.
 
-    install Install the software.
-            Example: manthys install <software_name>
+    install Install the software based on the software.json file. Software already installed will be reconfigured to use the cli version.
+            Example: manthys install
 
     help    Show this help message.
             Example: manthys help
@@ -21,7 +20,7 @@ Use "manthys <command> -h" for more information about a command.
 }
 
 func PrintCheckHelp() {
-    fmt.Println(`Check if the software is already installed and save the result in the software.json file. 
+    fmt.Println(`Runs an autocheck if the needed software is already installed and save the result in the software.json file. 
 				This json file is used by the install command based into what is already installed.`)
 }
 
