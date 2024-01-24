38aaf8cfce14410d8c2ae99e2586c3e59a1bfb05
manthysbr <121950847+manthysbr@users.noreply.github.com>
2024-01-24 18:21:00 -0300

    welcome screen

--- main.go
+++ main.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"os"
 	"manthys/cmd/validate" // Assegure-se de que esta é a localização correta do pacote
+	"manthys/cmd/utils"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "manthys",
-	Short: "Manthys é uma CLI para configurar rapidamente o seu ambiente de desenvolvimento",
-	Long:  `Uma ferramenta completa para a instalação e configuração de ambientes de desenvolvimento com suporte a múltiplas operações e verificações.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Bem-vindo ao Manthys CLI! Use 'manthys help' para ver os comandos disponíveis.")
-	},
-}
+    Use:   "manthys",
+    Short: "Manthys é uma CLI para configurar rapidamente o seu ambiente de desenvolvimento",
+    Long:  `Uma ferramenta completa para a instalação e configuração de ambientes de desenvolvimento com suporte a múltiplas operações e verificações.`,
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Println(utils.GetAsciiArt())
+        fmt.Println("Bem-vindo ao Manthys CLI! Use 'manthys help' para ver os comandos disponíveis.")
+    },
 
 func main() {
 	rootCmd.AddCommand(validate.CheckCmd)
