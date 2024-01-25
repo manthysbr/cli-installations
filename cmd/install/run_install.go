5c0edf5e371bf216f9d6d8582b02df2b826ddb43
Thiago "Manthys" Silva <121950847+manthysbr@users.noreply.github.com>
2024-01-25 18:01:58 -0300

    setting install corrections

--- cmd/install/run_install.go
+++ cmd/install/run_install.go
@@ -10,9 +10,9 @@ func RunInstall() {
     // Gera o relatório e pede confirmação
     if report.GenerateInstallationReport() {
         // O usuário confirmou, prossegue com a instalação
-        softwareState := config.LoadSoftwareState()
-        for software, state := range softwareState.Software {
-            if state == "Not installed" {
+        softwareState := config.GetSoftwareState()
+        for software, details := range softwareState.Software {
+            if details["state"] == "Not installed" {
                 // Chama a função correspondente para instalar o software
                 switch software {
                 case "Python":
@@ -26,9 +26,12 @@ func RunInstall() {
                 case "AzureCLI":
                     InstallAzureCLI()
                 }
+            } else {
+                fmt.Printf("%s is installed with version: %s\n", software, details["version"])
             }
         }
     } else {
-        fmt.Printf("%s is installed with version: %s\n", software, state)
+        // Mensagem caso o usuário opte por não instalar
+        fmt.Println("Instalação cancelada pelo usuário.")
     }
 }
