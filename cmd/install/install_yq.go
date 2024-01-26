8d26b7834d39e54c9c97d96c0ab1f5fa8675a449
Thiago "Manthys" Silva <121950847+manthysbr@users.noreply.github.com>
2024-01-26 12:39:30 -0300

    ytq correction

--- cmd/install/install_yq.go
+++ cmd/install/install_yq.go
@@ -1,27 +1,25 @@
 package install
 
 import (
-	"fmt"
-	//"log"
-	"os/exec"
-	//"time"
+    "fmt"
+    "os/exec"
 )
 
 func InstallYq() {
-	fmt.Println("YQ está sendo instalado. Aguarde...")
+    fmt.Println("YQ está sendo instalado. Aguarde...")
 
-	// Preparando ambiente
-	prepararAmbiente()
+    // Preparando ambiente
+    prepararAmbiente()
 
-	// Adiciona o PPA para o yq
-	addCmd := exec.Command("sudo", "add-apt-repository", "-y", "ppa:rmescandon/yq")
-	executeInstallCommand(addCmd, "Adicionando o repositório PPA para YQ...")
+    // Adiciona o PPA para o yq
+    addCmd := exec.Command("sudo", "add-apt-repository", "-y", "ppa:rmescandon/yq")
+    executeInstallCommand(addCmd)
 
-	// Atualiza os repositórios
-	updateCmd := exec.Command("sudo", "apt", "update")
-	executeInstallCommand(updateCmd, "Atualizando os repositórios...")
+    // Atualiza os repositórios
+    updateCmd := exec.Command("sudo", "apt", "update")
+    executeInstallCommand(updateCmd)
 
-	// Instala o yq
-	installCmd := exec.Command("sudo", "apt-get", "install", "-y", "yq")
-	executeInstallCommand(installCmd, "Instalando o YQ. Isso pode levar alguns instantes...")
+    // Instala o yq
+    installCmd := exec.Command("sudo", "apt-get", "install", "-y", "yq")
+    executeInstallCommand(installCmd)
 }
