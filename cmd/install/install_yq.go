98c93efe4b0d4c066f10cac65437e9d39ae7a515
Thiago "Manthys" Silva <121950847+manthysbr@users.noreply.github.com>
2024-01-26 11:01:52 -0300

    supporting common installs

--- cmd/install/install_yq.go
+++ cmd/install/install_yq.go
@@ -1,41 +1,12 @@
 package install
 
 import (
-    "bufio"
     "fmt"
-    "log"
     "os/exec"
-    "strings"
 )
 
 func InstallYq() {
     fmt.Println("Iniciando a instalação do yq...")
-
     cmd := exec.Command("sudo", "apt-get", "install", "-y", "yq")
     executeInstallCommand(cmd)
-}
-
-func executeInstallCommand(cmd *exec.Cmd) {
-    stdout, err := cmd.StdoutPipe()
-    if err != nil {
-        log.Fatalf("Erro ao criar o pipe para a saída padrão: %s", err)
-    }
-
-    if err := cmd.Start(); err != nil {
-        log.Fatalf("Falha ao iniciar a instalação: %s", err)
-    }
-
-    scanner := bufio.NewScanner(stdout)
-    for scanner.Scan() {
-        line := scanner.Text()
-        if strings.Contains(line, "Unpacking") || strings.Contains(line, "Setting up") {
-            fmt.Println(line)
-        }
-    }
-
-    if err := cmd.Wait(); err != nil {
-        log.Fatalf("Falha na instalação: %s", err)
-    } else {
-        fmt.Printf("%s instalado com sucesso\n", cmd.Args[len(cmd.Args)-1])
-    }
 }
