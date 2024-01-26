a8d988e78df62143d7cffc6571e8d3567b13385f
Thiago "Manthys" Silva <121950847+manthysbr@users.noreply.github.com>
2024-01-26 12:25:38 -0300

    corrections

--- cmd/install/install_yq.go
+++ cmd/install/install_yq.go
@@ -15,19 +15,13 @@ func InstallYq() {
 
 	// Adiciona o PPA para o yq
 	addCmd := exec.Command("sudo", "add-apt-repository", "-y", "ppa:rmescandon/yq")
-	executeCommand(addCmd, "Adicionando o repositório PPA para YQ...")
+	executeInstallCommand(addCmd, "Adicionando o repositório PPA para YQ...")
 
 	// Atualiza os repositórios
 	updateCmd := exec.Command("sudo", "apt", "update")
-	executeCommand(updateCmd, "Atualizando os repositórios...")
+	executeInstallCommand(updateCmd, "Atualizando os repositórios...")
 
 	// Instala o yq
 	installCmd := exec.Command("sudo", "apt-get", "install", "-y", "yq")
-	executeCommand(installCmd, "Instalando o YQ. Isso pode levar alguns instantes...")
-}
-
-func prepararAmbiente() {
-	// Simula o tempo de preparação do ambiente
-	time.Sleep(2 * time.Second)
-	fmt.Println("Preparando ambiente...")
+	executeInstallCommand(installCmd, "Instalando o YQ. Isso pode levar alguns instantes...")
 }
