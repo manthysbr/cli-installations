086ecb7405a8cf93c5d27e259cd5588548273709
Thiago "Manthys" Silva <121950847+manthysbr@users.noreply.github.com>
2024-02-20 16:41:30 -0300

    correctionz

--- cmd/install/common_install.go
+++ cmd/install/common_install.go
@@ -1,28 +1,28 @@
 package install
 
 import (
-	"bufio"
-	"fmt"
-	"github.com/briandowns/spinner"
-	"log"
-	"os/exec"
-	"time"
+    "bufio"
+    "fmt"
+    "github.com/briandowns/spinner"
+    "log"
+    "os/exec"
+    "time"
 )
 
-// executeInstallCommand executa um comando de instalação e fornece feedback ao usuário
-func executeInstallCommand(cmd *exec.Cmd) {
-	stdoutPipe, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatalf("Erro ao criar o pipe para a saída padrão: %s", err)
-	}
+// ExecuteInstallCommand executa um comando de instalação e fornece feedback ao usuário
+func ExecuteInstallCommand(cmd *exec.Cmd) {
+    stdoutPipe, err := cmd.StdoutPipe()
+    if err != nil {
+        log.Fatalf("Erro ao criar o pipe para a saída padrão: %s", err)
+    }
 
-	s := spinner.New(spinner.CharSets[39], 100*time.Millisecond) // Cria um novo spinner
-	s.Start()                                                   // Inicia o spinner
+    s := spinner.New(spinner.CharSets[39], 100*time.Millisecond) // Cria um novo spinner
+    s.Start()                                                   // Inicia o spinner
 
-	if err := cmd.Start(); err != nil {
-		s.Stop() // Para o spinner em caso de erro
-		log.Fatalf("Falha ao iniciar a instalação: %s", err)
-	}
+    if err := cmd.Start(); err != nil {
+        s.Stop() // Para o spinner em caso de erro
+        log.Fatalf("Falha ao iniciar a instalação: %s", err)
+    }
 
     // Novo: Cria um scanner para ler a saída do comando em tempo real
     scanner := bufio.NewScanner(stdoutPipe)
@@ -32,16 +32,17 @@ func executeInstallCommand(cmd *exec.Cmd) {
         }
     }()
 
-	s.Stop() // Para o spinner logo antes de esperar o comando terminar
+    s.Stop() // Para o spinner logo antes de esperar o comando terminar
 
-	if err := cmd.Wait(); err != nil {
-		log.Fatalf("Falha na instalação: %s", err)
-	} else {
-		fmt.Printf("%s instalado com sucesso\n", cmd.Args[len(cmd.Args)-1])
-	}
+    if err := cmd.Wait(); err != nil {
+        log.Fatalf("Falha na instalação: %s", err)
+    } else {
+        fmt.Printf("%s instalado com sucesso\n", cmd.Args[len(cmd.Args)-1])
+    }
 }
 
-func prepararAmbiente() {
+// PrepararAmbiente simula a preparação do ambiente
+func PrepararAmbiente() {
     fmt.Println("Preparando ambiente...")
 
     s := spinner.New(spinner.CharSets[39], 100*time.Millisecond) // Cria um novo spinner
